2f326d876f71f20363ae8b4fb59f3a519e646600
pvlbrzn <pavelberezan1998@gmail.com>
2024-12-30 22:31:40 +0300

    ready for deployment

--- mini_library/settings.py
+++ mini_library/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#^)mi3y34suf-t5+%ladmv4rm2xgy7slnyz22)m!3l1eo3)qxk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pvlbrzn.pythonanywhere.com']
 
 
 # Application definition
